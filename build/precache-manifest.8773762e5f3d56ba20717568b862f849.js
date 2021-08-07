4ab5820822eae6cba1b4f063b01edd249f8fed40
Arslan Jan Soomro <arslanjs.dev@gmail.com>
2021-08-06 23:35:38 -0700

    Imported Project From Heroku

---
+++ build/precache-manifest.8773762e5f3d56ba20717568b862f849.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ce2ba141d47fbb2b1d716e480591763",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3301fe1ca5646af1bbc3",
+    "url": "/static/css/main.a7d2d5d7.chunk.css"
+  },
+  {
+    "revision": "e1822f355832ea8d211b",
+    "url": "/static/js/2.0fe318f4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0fe318f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3301fe1ca5646af1bbc3",
+    "url": "/static/js/main.839c2ae5.chunk.js"
+  },
+  {
+    "revision": "c5f758ab953c6d6f2987",
+    "url": "/static/js/runtime-main.63661cca.js"
+  },
+  {
+    "revision": "2a47a29ceb33c966c8d79f8d5a5ea448",
+    "url": "/static/media/Poppins-Light.2a47a29c.ttf"
+  },
+  {
+    "revision": "ba95810b56f476990ca71d15139d5111",
+    "url": "/static/media/Poppins-Medium.ba95810b.ttf"
+  }
+]);
